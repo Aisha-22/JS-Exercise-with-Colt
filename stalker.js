0d4e6537227e026639dd95e2317cabb924eb56aa
Aisha <ash.hlatshwayo22@gmail.com>
2020-04-14 15:03:54 +0200

    Revision on my JS

---
+++ stalker.js
@@ -0,0 +1,15 @@
+var firstName = prompt("What is your name?");
+var lastName = prompt("What is your surname?")
+var age = prompt("What is your age?");
+
+// Colts Solution
+
+var fullName = firstName + " " + lastName;
+
+// alert("My name is " + firstName + " " + lastName);
+// alert("I am " + age + "" + " years old");
+
+// console.log("My name is " + firstName + " " + lastName);
+// Colts Solution 
+console.log("My names are " +  fullName);
+console.log("I am " + age + "" + " years old");
